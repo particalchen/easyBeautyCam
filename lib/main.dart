c49f2f29c374893b0af6c00be882b89d20d2186b
Patrick Chen <partical@PatrickdeMac-mini.local>
2026-06-04 18:12:53 +0800

    feat: 启动时同步远程姿势

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'app.dart';
+import 'features/pose_library/pose_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+
+  // 启动时同步远程姿势
+  final poseRepo = PoseRepository();
+  await poseRepo.syncRemotePoses();
+
   runApp(const ProviderScope(child: EasyBeautyCamApp()));
 }
